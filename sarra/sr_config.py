00e0ac053ba7e9e027f1506b10dc6435c0e558f7
petersilva <petersilva@sourceforge.net>
2018-11-20 01:08:33 -0500

    trying to get log files to update more often.

--- sarra/sr_config.py
+++ sarra/sr_config.py
@@ -970,6 +970,8 @@ class sr_config:
                 self.logger.error("sr_config/__on_heartbeat__ 3 Type: %s, Value: %s,  ..." % (stype, svalue))
                 self.logger.error( "plugin %s, execution failed." % plugin )
 
+        self.logger.flush()
+
         return True
 
     def get_exchange_option(self):
